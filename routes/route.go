cafa5b9440e4dcc73ba171769ad0622a09cae2f2
zakhaev26 <b422056@iiit-bh.ac.in>
2023-11-11 01:15:38 +0530

    feat(docs): adds documentation for apis

--- routes/route.go
+++ routes/route.go
@@ -8,6 +8,7 @@ import (
 func Router() *mux.Router {
 	r := mux.NewRouter()
 
+	
 	r.HandleFunc("/get-teamwise-players", controller.GetTeamWisePlayers).Methods("GET")
 	r.HandleFunc("/upload-player", controller.UploadTeamPlayer).Methods("POST")
 	return r
