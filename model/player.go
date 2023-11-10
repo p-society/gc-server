baa5f96c6eaaabffb63aff9bc6aabb5b37f003cb
zakhaev26 <b422056@iiit-bh.ac.in>
2023-11-11 01:32:43 +0530

    fix

--- model/player.go
+++ model/player.go
@@ -5,7 +5,6 @@ import (
 )
 
 // Player data structure (Acting as ORM ) for Basketball Players.
-
 type Player struct {
 	Name      string             `json:"name,omitempty"`
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
