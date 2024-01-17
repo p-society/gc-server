6c334eef833fc69281c81dc4b389e4c844c20c5e
Shreyansh <shreygupta799@gmail.com>
2024-01-17 16:00:05 +0530

    made basic UI changes in admin login page and player login page

--- apps/admin/lib/login_admin.dart
+++ apps/admin/lib/login_admin.dart
@@ -1,9 +1,5 @@
 import 'package:admin/textfield_login.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginPageAdmin extends StatelessWidget {
   const LoginPageAdmin({super.key});
--- apps/admin/lib/main.dart
+++ apps/admin/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:admin/login_admin.dart';
 import 'package:admin/login_player.dart';
+import 'package:admin/otp_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPagePlayer(),
+      home: const OtpPage(),
     );
   }
 }
