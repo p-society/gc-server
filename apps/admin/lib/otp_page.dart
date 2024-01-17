6199bcef5e03c60693581bef3a53b79092b1f1cc
Shreyansh <shreygupta799@gmail.com>
2024-01-17 15:58:22 +0530

    created OTP page

---
+++ apps/admin/lib/otp_page.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+
+class OtpPage extends StatelessWidget {
+  const OtpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: const TextStyle(
+          fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600),
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+
+    final focusedPinTheme = defaultPinTheme.copyDecorationWith(
+      border: Border.all(color: const Color.fromRGBO(114, 178, 238, 1)),
+      borderRadius: BorderRadius.circular(8),
+    );
+
+    final submittedPinTheme = defaultPinTheme.copyWith(
+      decoration: defaultPinTheme.decoration?.copyWith(
+          // color: Color.fromRGBO(234, 239, 243, 1),
+          ),
+    );
+
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            //aplying gradient
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF111114),
+              Color(0xFF161A3A),
+              Color(0xFF171D45),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 255,
+              ),
+              const Text(
+                'Enter code send to your E-mail',
+                style: TextStyle(
+                  color: Color(0xFFE1DADE),
+                  fontSize: 25,
+                  fontFamily: 'Open Sans',
+                  fontWeight: FontWeight.w700,
+                  height: 0,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Text(
+                'We have sent the code to abc@gmail.com',
+                style: TextStyle(
+                  color: Color(0xFFD3CFCF),
+                  fontSize: 15,
+                  fontFamily: 'Open Sans',
+                  fontWeight: FontWeight.w500,
+                  height: 0,
+                ),
+              ),
+              const SizedBox(
+                height: 43,
+              ),
+              Pinput(
+                defaultPinTheme: defaultPinTheme,
+                focusedPinTheme: focusedPinTheme,
+                submittedPinTheme: submittedPinTheme,
+                validator: (s) {
+                  return s == '2222' ? null : 'OTP is incorrect';
+                },
+                closeKeyboardWhenCompleted: true,
+                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                showCursor: true,
+                onCompleted: (pin) => print(pin),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Didn\'t recieve the code?  ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontFamily: 'Open Sans',
+                      fontWeight: FontWeight.w500,
+                      height: 0,
+                    ),
+                  ),
+                  GestureDetector(
+                    //making signup text clickabe
+                    onTap: () {
+                      //print('signup clicked');
+                    },
+                    child: const Text(
+                      'Resend',
+                      style: TextStyle(
+                        color: Color(0xFFC01A60),
+                        fontSize: 15,
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
