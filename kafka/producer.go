f0268b356e88449051745fc29b042aaf0135e8dd
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-03-10 21:45:52 +0530

    feat:adds kafka modules for pub/sub

---
+++ kafka/producer.go
@@ -0,0 +1,47 @@
+package kafka
+
+import (
+	"fmt"
+
+	"github.com/IBM/sarama"
+)
+
+func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func PushToKafka(topic string, message string) error {
+
+	brokersUrl := []string{"localhost:9092"}
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+
+	_, _, err = producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Message Produced!\n")
+
+	return nil
+}
